49fa9b2f7752fd17c1a22d4a167856bb9d3cbef4
Ivan <110199ws@gmail.com>
2025-04-01 16:08:27 +0300

    generic tasks solution

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,12 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare
+  <
+  T extends Pick<AllType, 'name' | 'color'>,
+  B extends Pick<AllType, 'position' | 'weight'>
+  >
+  (top: T, bottom: B): AllType {
   return {
     name: top.name,
     color: top.color,
--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,8 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
-  return Object.assign(objA, objB);
+function merge <A, B>(objA: A, objB: B):A & B{
+  return Object.assign({}, objA, objB);
 }
 
 export {}
